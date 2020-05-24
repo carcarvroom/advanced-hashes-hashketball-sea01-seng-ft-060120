5938d4f5a29151f7f38fb6a7deb2cfe3fa75ee1a
Carly L <carlylynnla@gmail.com>
2020-05-24 17:33:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,4 +126,30 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(player_name)
+  game_hash
+end
+
+def shoe_size 
+  
+end
+
+def team_colors
+  
+end
+
+def team_names
+  
+end
+
+def player_numbers
+  
+end
+
+def player_stats
+  
+end
+
+def big_shoe_rebounds
+  
+end
